bc07bc45e616304c294c04be964990ce1f3a5199
ConstintCreations <ethanglenn1208@gmail.com>
2026-04-17 19:28:38 -0500

    Register Brython code with register request

---
+++ static/register.py
@@ -0,0 +1,65 @@
+from browser import document, aio
+from browser.timer import set_timeout, clear_timeout
+from browser.local_storage import storage
+import json
+
+register_button = document.select_one(".register-name-button")
+register_name = document.select_one(".register-name")
+register_info = document.select_one(".register-info")
+
+hide_info_timer = None
+username = ""
+
+def hide_register_info():
+    register_info.style.display = "none"
+    register_info.text = ""
+
+def show_register_info(text:str = "Oops, it looks like I haven't implemented this yet! :c", hide_timer:bool = True):
+    global hide_info_timer
+    register_info.text = text
+    register_info.style.display = "block"
+
+    if hide_info_timer:
+        clear_timeout(hide_info_timer)
+    if hide_timer:
+        hide_info_timer = set_timeout(hide_register_info, 5000)
+
+
+async def register_button_click(event):
+    global username
+
+    username = register_name.value.strip()
+
+    if not(username):
+        show_register_info("Please enter a username.")
+        return
+
+    await register_user()
+
+async def register_user():
+    global username
+    request = await aio.post(
+        "/register",
+        headers={
+            "Content-Type": "application/json"
+        },
+        data=json.dumps({"name": username})
+    )
+
+    data = json.loads(request.data)
+    print(request.status, data)
+
+    if request.status == 200:
+        storage["api_key"] = data['api_key']
+        show_register_info(text = f"Success! You are now logged in with your new API Key: {data['api_key']}", hide_timer = False)
+    else:
+        if request.status == 400:
+            show_register_info(text = "Oops! There is alread a user with the same username. Try again with a different name.")
+        else:
+            show_register_info(text = f"Oh No! An unknown error occured. ({request.status})")
+
+
+def register_button_click_handler(ev):
+    aio.run(register_button_click(ev)) 
+
+register_button.bind("click", register_button_click_handler)
